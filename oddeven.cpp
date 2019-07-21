c142c51f26097687a85ed93a606cd3e7120d1066
alwinfranco <53120905+alwinfranco@users.noreply.github.com>
2019-07-21 11:54:35 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"  ";
+cin>>a;
+{
+    if(a%2==0)
+    {
+        cout<<"even";
+    }    
+    else
+    {
+        cout<<"odd";
+    }
+}    
+} 
