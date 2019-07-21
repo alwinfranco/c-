3e8ba853fd4dfb0e0ff6efb351123d5903b1c9c7
alwinfranco <53120905+alwinfranco@users.noreply.github.com>
2019-07-21 11:30:28 +0530

    Create posneg.cpp

---
+++ posneg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"  ";
+cin>>a;
+{
+    if(a>0)
+      cout<<"positive";
+    if(a<0)
+      cout<<"negative";
+    if(a==0)
+      cout<<"zero";
+    
+}
+}
